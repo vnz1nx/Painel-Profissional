d73f90596d75227dea1cef58103b563e6ab9d7d3
VNZ1nx <125399839+vnz1nx@users.noreply.github.com>
2025-02-06 14:07:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll("#mobile-nav a");
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileNav = document.getElementById('mobile-nav');
+const sidebar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".sidebar-nav");
 
-    menuToggle.addEventListener('click', () => {
-        mobileNav.classList.toggle('active');
+menu.addEventListener("click", () =>{
+    sidebar.classList.toggle("collapsed");
+});
+
+navLinks.addEventListener("click", () => {
+        sidebar.classList.remove("collapsed");
     });
 
-    // Fecha o menu ao clicar em qualquer link dentro dele
-    navLinks.forEach(link => {
-        link.addEventListener("click", function () {
-            mobileNav.classList.remove('active'); // Fecha o menu ao clicar no link
-        });
-    });
-
-    // Garante que o menu sempre feche ao redimensionar para uma tela maior
-    window.addEventListener("resize", function () {
-        if (!isMobile()) {
-            mobileNav.classList.remove('active'); // Remove a classe "active" em telas grandes
+function filterProjects(tech) {
+    const projects = document.querySelectorAll('.project-card');
+    projects.forEach(project => {
+        if (tech === 'all' || project.classList.contains(tech)) {
+            project.style.display = 'inline-block';
+        } else {
+            project.style.display = 'none';
         }
     });
-});
+}
